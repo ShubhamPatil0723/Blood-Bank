fc967bea8b6d7d2454be46c7befffa58ed4077d9
Shubham Patil <shubhampatil0723@gmail.com>
2023-02-10 11:29:45 +0530

    migrated connection from sqlite to mysql

--- models/index.js
+++ models/index.js
@@ -2,10 +2,16 @@ import Sequelize, { DataTypes } from "sequelize";
 import { generateModels } from "./generateModels.js";
 const db = {};
 
-const sequelizeInit = new Sequelize({
-  dialect: "sqlite",
-  storage: "./Dummy.sqlite3",
-});
+const { MYSQL_USERNAME, MYSQL_PASSWORD } = process.env;
+const sequelizeInit = new Sequelize(
+  "bloodbank",
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 db.models = generateModels(sequelizeInit, DataTypes);
 db.Sequelize = Sequelize;
